4e93e06fba6503369f2953712fdd64d5044218b9
noahhhi <noahhhhm@gmail.com>
2026-08-12 08:58:10 +0800

    Update HoRNDIS to 0.2.3

--- Formula/horndis.rb
+++ Formula/horndis.rb
@@ -1,8 +1,8 @@
 class Horndis < Formula
   desc "Entitlement-free Android RNDIS USB tethering for modern macOS"
   homepage "https://github.com/noahhhi/HoRNDIS-Userspace"
-  url "https://github.com/noahhhi/HoRNDIS-Userspace/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "2b0b1b17a49fa192fab502a1df2f0e5fbad985a30f6e1421332ed5ac991021df"
+  url "https://github.com/noahhhi/HoRNDIS-Userspace/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "b5b60b4375504cc2fc7b9a1c82ee60524861176a24fc63bc66eb71897c99f5b6"
   license "GPL-3.0-or-later"
   head "https://github.com/noahhhi/HoRNDIS-Userspace.git", branch: "main"
 
